f6679967f007bc555f181271335ef2af82e9fb5b
Таячинов Данил <dat59@tpu.ru>
2025-04-30 18:53:53 +0700

    Единая БД для сервисов

--- user_service/settings.py
+++ user_service/settings.py
@@ -5,7 +5,7 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default= "postgresql+asyncpg://postgres:adminadmin@localhost/BazarTPU_fastapi"
+    default= "postgresql+asyncpg://postgres:postgres@localhost:5433/BazarTPU"
 )
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
